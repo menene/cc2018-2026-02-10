892f4bd3dba8ba0fae204496de87bab3517209b3
menene <efmarroquinr@gmail.com>
2026-07-30 18:27:34 -0600

    fix

--- src/player.rs
+++ src/player.rs
@@ -1,8 +1,6 @@
 use nalgebra_glm::Vec2;
 
 pub struct Player {
-    /// Posición del jugador en el mundo 2D, en píxeles.
     pub pos: Vec2,
-    /// Ángulo de vista, en radianes.
     pub a: f32,
 }
